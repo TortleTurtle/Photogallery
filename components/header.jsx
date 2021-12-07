bd57b104ad0f395b411aa63013f2fbec1ee67bfa
TortleTurtle <danielmeijs@gmail.com>
2021-12-07 21:47:13 +0100

    Added hover colour to nav links. Colour is now in its own file.

--- components/header.jsx
+++ components/header.jsx
@@ -13,24 +13,31 @@ export class Header extends React.Component {
         this.setState({
             toggle: !this.state.toggle
         });
-        //TODO: Add burger animation
     }
 
     render() {
         return (
             <header className={styles.header}>
                 <div className={styles.logoContainer}>
-                    <h3>Lorem Ipsum</h3>
+                    <h3> Lorem Ipsum</h3>
                 </div>
-                <nav className={`${styles.nav} ${this.state.toggle ? styles.navToggled : ""}`}>
-                    <a href={""}>Home</a>
-                    <a href={""}>Galleries</a>
-                    <a href={""}>Contact</a>
+                <nav className={`${styles.nav} ${this.state.toggle ? styles["nav--toggled"] : ""}`}>
+                    <ul>
+                        <li>
+                            <a href={""}>Home</a>
+                        </li>
+                        <li>
+                            <a href={""}>Galleries</a>
+                        </li>
+                        <li>
+                            <a href={""}>Contact</a>
+                        </li>
+                    </ul>
                 </nav>
-                <div className={styles.burger} onClick={() => this.toggle()}>
-                    <div className={styles.line}/>
-                    <div className={styles.line}/>
-                    <div className={styles.line}/>
+                <div className={`${styles.burger} ${this.state.toggle ? styles["burger--toggled"] : ""}`} onClick={() => this.toggle()}>
+                    <div className={`${styles.line} ${this.state.toggle ? styles["line--toggled"] : ""}`}/>
+                    <div className={`${styles.line} ${this.state.toggle ? styles["line--toggled"] : ""}`}/>
+                    <div className={`${styles.line} ${this.state.toggle ? styles["line--toggled"] : ""}`}/>
                 </div>
             </header>
         )

311cee96ad3d4adecedbf7964880934a4869057f
TortleTurtle <danielmeijs@gmail.com>
2022-01-14 17:33:15 +0100

    Added logging to config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
+const config = {
+  strapi_url: process.env.STRAPI_URL,
+  public_strapi_url: process.env.NEXT_PUBLIC_STRAPI_URL,
+  domain: process.env.NEXT_PUBLIC_DOMAIN
+}
+
+console.log("Next JS env variables:");
+console.log(config);
+
 module.exports = {
   reactStrictMode: true,
   images: {

3be02ea533169da770c5b1cc0fd60ce169ad98b8
TortleTurtle <danielmeijs@gmail.com>
2022-01-14 17:54:44 +0100

    config now excepts a env variable for domains.

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
-const config = {
+const env_variables = {
   strapi_url: process.env.STRAPI_URL,
   public_strapi_url: process.env.NEXT_PUBLIC_STRAPI_URL,
   domain: process.env.NEXT_PUBLIC_DOMAIN
 }
 
+const domains = (process.env.IMG_DOMAIN) ? [process.env.IMG_DOMAIN] : ['localhost'];
 console.log("Next JS env variables:");
-console.log(config);
+console.log(env_variables);
 
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost'],
+    domains: domains,
   },
 }

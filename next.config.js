4c0a77d993bb697cee48a5f993736e71abd46bd0
TortleTurtle <danielmeijs@gmail.com>
2022-01-19 14:42:30 +0100

    added basePath to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 const env_variables = {
   strapi_url: process.env.STRAPI_URL,
   public_strapi_url: process.env.NEXT_PUBLIC_STRAPI_URL,
-  domain: process.env.NEXT_PUBLIC_DOMAIN
+  next_domain: process.env.NEXT_PUBLIC_DOMAIN,
+  image_domain: process.env.IMG_DOMAIN,
+  base_path: process.env.BASE_PATH
 }
 
 const domains = (process.env.IMG_DOMAIN) ? [process.env.IMG_DOMAIN] : ['localhost'];
@@ -13,4 +15,5 @@ module.exports = {
   images: {
     domains: domains,
   },
+  basePath: process.env.BASE_PATH,
 }

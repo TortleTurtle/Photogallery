c5e49b843acdb8c2f217c920058a77c2c58dbc96
TortleTurtle <danielmeijs@gmail.com>
2022-01-26 16:03:42 +0100

    Hard coded domains.

--- next.config.js
+++ next.config.js
@@ -1,18 +1,15 @@
 const env_variables = {
   strapi_url: process.env.STRAPI_URL,
   public_strapi_url: process.env.NEXT_PUBLIC_STRAPI_URL,
-  next_url: `${process.env.NEXT_PUBLIC_URL}`,
-  image_domain: process.env.APP_URL,
-  base_path: process.env.BASE_PATH
+  next_url: `${process.env.NEXT_PUBLIC_URL}`
 }
 
-const domains = (process.env.APP_URL) ? [process.env.APP_URL] : ['localhost'];
 console.log("Next JS env variables:");
 console.log(env_variables);
 
 module.exports = {
   reactStrictMode: false,
   images: {
-    domains: domains,
+    domains: ['cheftags.nl', 'localhost', 'strapi-3o8lh.ondigitalocean.app'],
   },
 }
